c151bca60909fa04d4a3693a33ccc285bc685727
Brian Marquis <rbmarquis.dev@gmail.com>
2018-07-29 18:47:18 -0500

    removed unnecessary default create-react-app stuff, setup initial grid

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Header</header>
+        <main className="App-main">Main
+        </main>
+        <footer className="App-footer">Footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
