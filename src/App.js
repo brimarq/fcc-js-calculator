03b86df68067acf6685ee8a0cfcf5e6730db0e9c
Brian Marquis <rbmarquis.dev@gmail.com>
2018-07-31 20:08:28 -0500

    render display and each calckey as components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,82 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// ÷
+// DIVISION SIGN
+// Unicode: U+00F7, UTF-8: C3 B7
+
+// ×
+// MULTIPLICATION SIGN
+// Unicode: U+00D7, UTF-8: C3 97
+
+// −
+// MINUS SIGN
+// Unicode: U+2212, UTF-8: E2 88 92
+
+const calcKeys = [
+  { id: "multiply", face: "×", value: "*" },
+  { id: "divide", face: "÷", value: "/" },
+  { id: "add", face: "+", value: "+" },
+  { id: "subtract", face: "−", value: "-" },
+  { id: "seven", face: "7", value: "7" },
+  { id: "eight", face: "8", value: "8" },
+  { id: "nine", face: "9", value: "9" },
+  { id: "clear", face: "AC", value: "?" },
+  { id: "four", face: "4", value: "4" },
+  { id: "five", face: "5", value: "5" },
+  { id: "six", face: "6", value: "6" },
+  { id: "one", face: "1", value: "1" },
+  { id: "two", face: "2", value: "2" },
+  { id: "three", face: "3", value: "3" },
+  { id: "equals", face: "=", value: "=" },
+  { id: "zero", face: "0", value: "0" },
+  { id: "decimal", face: ".", value: "." }
+]
+
+class CalcKey extends Component {
+  render() {
+    return (
+      <div id={this.props.id} className="calckey">{this.props.face}</div>
+    );
+  }
+}
+
+class Display extends Component {
+  render() {
+    return (
+      <div id="display">display</div>
+    );
+  }
+}
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div id="calculator">
+
+        <Display />
+
+        { calcKeys.map(calcKey => (
+          <CalcKey 
+            key={calcKey.id} 
+            id={calcKey.id} 
+            face={calcKey.face} 
+            value={calcKey.value} 
+          />
+        ))}
+
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">Header</header>
         <main className="App-main">
-
-          <div id="calculator">
-
-            <div id="display">display</div>
-            <div id="multiply" className="calckey operator">x</div>
-            <div id="divide" className="calckey operator">/</div>
-            <div id="add" className="calckey operator">+</div>
-            <div id="subtract" className="calckey operator">-</div>
-            <div id="seven" className="calckey num">7</div>
-            <div id="eight" className="calckey num">8</div>
-            <div id="nine" className="calckey num">9</div>
-            <div id="clear" className="calckey clear">AC</div>
-            <div id="four" className="calckey num">4</div>
-            <div id="five" className="calckey num">5</div>
-            <div id="six" className="calckey num">6</div>
-            <div id="one" className="calckey num">1</div>
-            <div id="two" className="calckey num">2</div>
-            <div id="three" className="calckey num">3</div>
-            <div id="equals" className="calckey equals">=</div>
-            <div id="zero" className="calckey num">0</div>
-            <div id="decimal" className="calckey num">.</div>
-
-          </div>
+          <Calculator />
         </main>
         <footer className="App-footer">Footer</footer>
       </div>
