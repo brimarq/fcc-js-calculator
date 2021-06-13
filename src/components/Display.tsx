d53b6dc2ccd65ddc29426880bf63bd179b2b0c22
Brian Marquis <rbmarquis.dev@gmail.com>
2021-06-12 21:24:19 -0500

    convert Display to TS

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Display.css';
 
-class Display extends React.PureComponent {
+class Display extends React.PureComponent<{ display: string }> {
   render() {
     return <div id="display">{this.props.display}</div>;
   }
