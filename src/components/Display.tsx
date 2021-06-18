2d6aea3eeafe56065e2ccc4f1fee7c939ed02a23
Brian Marquis <rbmarquis.dev@gmail.com>
2021-06-18 18:56:50 -0500

    change to functional component

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Display.css';
 
-class Display extends React.PureComponent<{ display: string }> {
-  render() {
-    return <div id="display">{this.props.display}</div>;
-  }
+// class Display extends React.PureComponent<{ display: string }> {
+//   render() {
+//     return <div id="display">{this.props.display}</div>;
+//   }
+// }
+
+function Display({ display }: { display: string }) {
+  return <div id="display">{display}</div>;
 }
 
 export default Display;
