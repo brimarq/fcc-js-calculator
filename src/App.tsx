7fa0d8fa45a0228e6894f420d830c85e0e39b1f4
Brian Marquis <rbmarquis.dev@gmail.com>
2021-06-18 19:01:40 -0500

    change to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header"></header>
-        <main className="App-main">
-          <Calculator />
-        </main>
-        <footer className="App-footer"></footer>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main className="App-main">
+        <Calculator />
+      </main>
+      <footer className="App-footer"></footer>
+    </div>
+  );
 }
 
 export default App;
